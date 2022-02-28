b5b283e01d0dcfa26925bbd6316827db7da5a508
Jean Pedro <jeanjesuspedrobook@gmail.com>
2022-02-28 17:24:46 -0300

    Update  normal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 ScrollReveal({ reset: true });
 
+
 ScrollReveal().reveal('.introducao-main', { delay: 500 });
 ScrollReveal().reveal('.personal-details', { delay: 700 });
 ScrollReveal().reveal('.interests', { delay: 900 });

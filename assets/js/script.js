ed15f4eb3a717d2a1b5ae7cdc3657d35fcec28fa
Jean Pedro <jeanjesuspedrobook@gmail.com>
2022-02-27 22:09:00 -0300

    Update Main

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.introducao-main', { delay: 500 });
+ScrollReveal().reveal('.personal-details', { delay: 700 });
+ScrollReveal().reveal('.interests', { delay: 900 });

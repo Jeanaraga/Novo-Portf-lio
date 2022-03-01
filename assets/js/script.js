0bb49fd4cd488885582cbf03615ecc37dae1fad4
Jean Pedro <jeanjesuspedrobook@gmail.com>
2022-03-01 20:27:29 -0300

    Update - Segunda parte Terminada

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,6 @@ ScrollReveal({ reset: true });
 ScrollReveal().reveal('.introducao-main', { delay: 500 });
 ScrollReveal().reveal('.personal-details', { delay: 700 });
 ScrollReveal().reveal('.interests', { delay: 900 });
+
+ScrollReveal().reveal('.titulo-secundary', { delay: 500 });
+ScrollReveal().reveal('.conteudo-secundary', { delay: 800 });

9ace55da47fdfdf9086187e377b59dd5fa64cbb5
Jean Pedro <jeanjesuspedrobook@gmail.com>
2022-03-31 17:05:09 -0300

    feat(alteração no smoth behavior)

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,31 @@ ScrollReveal().reveal('.conteudo-secundary', { delay: 800 });
 ScrollReveal().reveal('.container-third', { delay: 400 });
 
 ScrollReveal().reveal('.container-footer', { delay: 300 });
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.links-internos a[href^="a"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+    
+        section.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
+    
+        // Forma alternativa
+        // const topo = section.offsetTop;
+        // window.scrollTo({
+        //   top: topo,
+        //   behavior: 'smooth',
+        // })
+      }
+    
+      linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
+      })
+}
+
+initScrollSuave();
